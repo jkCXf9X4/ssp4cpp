9b7b66a6af5206943a156448ceeda56a32ecaa2a
Erik Rosenlund <erik.rosenlund@saabgroup.com>
2024-09-16 15:56:35 +0200

    graph tests

--- test/graph_analysis/main.cpp
+++ test/graph_analysis/main.cpp
@@ -47,17 +47,31 @@ int main()
     myfile.close();
 
     // Parsing FMI
+    auto fmus = vector<fmiHandle*>();
 
-    auto fmu_path = ssp.resources[0].file.c_str();
+    for (int i = 0; i < ssp.resources.size(); i++)
+    {   
+        auto r = ssp.resources[i];
+        auto fmu = fmi4c_loadFmu(r.file.c_str(), r.name.value().c_str());
 
-    fmiHandle *fmu = fmi4c_loadFmu(fmu_path, "myfmu");
-    cout << "Version " << fmi4c_getFmiVersion(fmu) << endl;
+        if (!fmu)
+        {
+            printf("Failed to load FMU\n");
+            exit(1);
+        }
+        cout << "Creating FMU " << i << " : " << r.name.value_or("null") << endl;
+        cout << "Version " << fmi4c_getFmiVersion(fmu) << endl;
 
-    if (!fmu)
-    {
-        printf("Failed to load FMU\n");
-        exit(1);
+        fmus.push_back(fmu);
     }
+
+    for (auto fmu : fmus)
+    {
+        cout << fmi2_getModelName(fmu) << endl;
+        // cout << fmi2_getNumberOfModelStructureOutputs(fmu) << endl;
+        // fmi4c_freeFmu(fmu);
+    }
+    return 0;
 
     
     map<string, int> connector_map;
@@ -73,7 +87,7 @@ int main()
                 {
                     auto name = component.name.value() + "." + connector.name;
                     connector_map[name] = connector_index++;
-                    cout << name << endl;
+                    // cout << name << endl;
                 }
             }
         }
@@ -84,7 +98,6 @@ int main()
 
     cout << "Connectors: " << connector_map.size() << endl;
     Graph g(connector_map.size());
-
 
     for (auto [name, index] : connector_map)
     {
@@ -97,10 +110,12 @@ int main()
         auto end = connection.endElement.value() + "." + connection.endConnector;
 
         add_edge(connector_map[start], connector_map[end], g);
-        cout << connector_map[start] << " -> " << connector_map[end] << endl;
+        // cout << connector_map[start] << " -> " << connector_map[end] << endl;
     }
 
     boost::write_graphviz(std::cout, g, make_label_writer(get(boost::vertex_name_t(), g)));
+
+
 
 
     //     startElement: AdaptionUnit
